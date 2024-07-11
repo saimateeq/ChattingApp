b1529302e7717b50e88b7c132e43e9470f715035
saimateeq <143260869+saimateeq@users.noreply.github.com>
2024-07-11 18:29:02 +0500

    Update website content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import  { BrowserRouter, Route, Routes } from "react-router-dom"
+import  { HashRouter, Route, Routes } from "react-router-dom"
 import WelcomePage from './pages/WelcomePage'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
@@ -8,15 +8,15 @@ import ChattingPage from './pages/ChattingPage'
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Routes>
+    <HashRouter>
+    <Routes >
       <Route path='/' Component={WelcomePage}/>
       <Route path='/LoginPage' Component={LoginPage}/>
       <Route path='/SignUpPage' Component={SignUpPage}/>
       <Route path='/MainBg' Component={MainBg}/>
       <Route path='/ChattingPage' Component={ChattingPage}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
